bb9f0d91f9fc0454e3d50e8b86827ca09d5fe1ff
yeonjukko <yeonju5868@gmail.com>
2016-06-18 17:32:20 +0900

    통계 기능 추가

--- routes/redirectUrl.js
+++ routes/redirectUrl.js
@@ -3,11 +3,45 @@
  */
 var https = require('https');
 var requestUrl = 'market://details?id=net.yeonjukko.bodyend';
+var pool = require('../libs/DBManager').mysqlPool;
 
-exports.redirectUrl = function(req, res){
+exports.redirectUrl = function (req, res) {
     res.redirect(requestUrl);
 
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            console.log(err);
+        } else {
+            var cafeName = req.query.cafeName;
+            connection.query('INSERT INTO BODY_END_ANALYSIS VALUES(?,?,?)'
+                , [myDate(), cafeName,myTime()], function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+        }
+        connection.release();
+    });
 };
+
+function myDate() {
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = d.getMonth() + 1;
+    var date = d.getDate();
+
+    if (month < 10) {
+        month = '0' + month;
+    }
+    if (date < 10) {
+        date = '0' + date;
+    }
+
+    return Number(year + month + date);
+}
+function myTime(){
+    return Number(new Date().getHours());
+}
 
 
 
